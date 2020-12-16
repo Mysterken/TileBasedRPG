ee8df610ed8f848a8c2d4dba21b97b7404ec5343
Mysterken <Mysterkenzo@gmail.com>
2020-12-16 14:57:04 +0100

    Added map dir
    map directory created to put all map/tileset file => tsx/tmx
    Better visibility in root

---
+++ map/Water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Water" tilewidth="32" tileheight="32" tilecount="384" columns="64">
+ <image source="../img/tileset/[A]Water2_pipo.png" width="2048" height="192"/>
+</tileset>
