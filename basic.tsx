538e55218331b9db0eec4a7d4cc8e43cd80bf4c7
Mysterken <Mysterkenzo@gmail.com>
2020-12-16 14:53:15 +0100

    Delete basic.tsx
    new dir => map

--- basic.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="basic" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
- <image source="../../Tileset/Pipoya RPG Tileset 32x32/[Base]BaseChip_pipo.png" width="256" height="4256"/>
-</tileset>
